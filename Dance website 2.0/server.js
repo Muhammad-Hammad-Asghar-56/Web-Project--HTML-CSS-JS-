b2b424e5998ffebe9a33ef83ca9d5e664ef73843
Muhammad Hammad Asghar <101011203+Muhammad-Hammad-Asghar-56@users.noreply.github.com>
2022-08-10 11:45:12 +0500

    Node.JS + PUG and Web Development

---
+++ Dance website 2.0/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app=express();
+const path = require('path');
+//   set port
+const port=8000;
+app.use('/static', express.static('static'));
+
+//______________________ PUG Settings ______________________________
+// set the PUG to the view template engine
+app.set('view engine', 'pug');
+// Set the views directory
+app.set('views', path.join(__dirname, 'views'));
+app.get('/',(req,res)=>{
+    res.status(200).render('index.pug',{});
+})
+app.post('/',()=>{
+    console.log("Something is post");
+})
+//__________________________________________________________________
+
+app.listen(port,()=>{
+    console.log("Server is on At 80");
+})
