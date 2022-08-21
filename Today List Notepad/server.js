ed733eb160b55a15ddc2c96f07dcc4a7bc35243b
Muhammad Hammad Asghar <101011203+Muhammad-Hammad-Asghar-56@users.noreply.github.com>
2022-08-21 16:08:29 +0500

    Database with MongoDb and single list

--- Today List Notepad/server.js
+++ Today List Notepad/server.js
@@ -1,51 +1,98 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const mongoose = require('mongoose');
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'pug');
-app.use("/static",express.static("static"));
+app.use("/static", express.static("static"));
 const port = 3000;
-var items=['Buy Food','Cook Meal','Eat'];
+let ItemArray = [];
 
+//_________________________________________________________________________________
+//                            DATABASE Settings
+mongoose.connect("mongodb://127.0.0.1:27017/todoListDB");
+
+//Get the default connection
+const db = mongoose.connection;
+
+//  add schema of the new blog
+const toDoSchema = mongoose.Schema({
+    toDoItem: {
+        type: String,
+        required: true,
+    }
+})
+//  get model from the schema
+
+
+
+const toDoModel = mongoose.model("todolists", toDoSchema);
+
+//_________________________________________________________________________________
+//                          Routing
+//_________________________________________________________________________________
 app.get('/', (req, res) => {
     var today = new Date();
-
-    //              1st Method
-    
-    // const dayNames = ['Sunday', 'Monday', 'TuesDay', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-    // const monthNames = ["January", "February", "March", "April", "May", "June","July", "August", "September", "October", "November", "December"];
-    // const month = today.getMonth();
-    // const parmas = {
-        //     title: 'Notepad',
-        //     day: dayNames[today.getDay()],
-        //     month:monthNames[today.getMonth()],
-        //     date:today.getDate(),
-        // }
-
-    //              2nd Method
-
     var options = { weekday: 'long', month: 'long', day: 'numeric' };
-    const parmas={
-        Day:today.toLocaleDateString('en-US',options),
-        item:items
-    }
+
     
-    // console.log(today.toLocaleDateString('en-US',options));
-    res.render('index.pug', parmas);
+    toDoModel.find((err, itemsList) => {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            const parmas = {
+                Day: today.toLocaleDateString('en-US', options),
+                item: itemsList
+            }
+            res.render('index.pug', parmas);
+        }
+    });
+
+
 })
 
-
-app.post('/',(req,res)=>{
-    var newItem=req.body.textInput;
-    if(newItem!=""){
-        items.push(newItem); 
+app.post('/', (req, res) => {
+    var newItem = req.body.textInput;
+    if (newItem != "" && (! isPresentInList(newItem))) {
+        let newToDoItem= new toDoModel({toDoItem:newItem});
+        newToDoItem.save();
     }
-    console.log(items);
     res.redirect('/');
 })
+app.post('/delete', (req, res) => {
+    console.log(req.body);
+    const id=req.body.delete;
+    toDoModel.findByIdAndRemove(id,(err)=>{
+        if(err){
+            console.log(err);
+        }
+        else {
+            console.log("Delete Sucessively");
+            res.redirect('/');
+        }
+    })
 
+})
 
+//_________________________________________________________________________
+//                          Validations
+//_________________________________________________________________________
+function isPresentInList(item){
+    toDoModel.find((err, itemsList) => {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            itemsList.forEach(Element=>{
+                if (Element === item){
+                    return true ;
+                }
+            })
+        }
+    });
+    return false;
+}
 app.listen(port, () => {
     console.log("Server is running at " + port);
 })
